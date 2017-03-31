93c4e45cb85cf5e98c9f8972385fa1257fda03e6
admin <admin>
2017-03-31 21:12:33 +0500

    made graph responsive

--- angular-src/src/app/services/validation.service.ts
+++ angular-src/src/app/services/validation.service.ts
@@ -20,9 +20,9 @@ export class ValidationService {
   dateDiffInDays(a, b) {
     var MS_PER_DAY = 1000 * 60 * 60 * 24;
   // Discard the time and time-zone information.
-  var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-  var days = Math.floor((utc2 - utc1) / MS_PER_DAY);
-  return days;
+    var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    var days = Math.floor((utc2 - utc1) / MS_PER_DAY);
+    return days;
 }
 }
